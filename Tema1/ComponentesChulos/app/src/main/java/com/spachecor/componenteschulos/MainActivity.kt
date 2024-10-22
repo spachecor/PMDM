ed85711ef8ba9b887a73c75b43c22223f23e8838
Selene <spachecor03@gmail.com>
2024-10-22 14:13:56 +0200

    maniana 2210204

--- Tema1/ComponentesChulos/app/src/main/java/com/spachecor/componenteschulos/MainActivity.kt
+++ Tema1/ComponentesChulos/app/src/main/java/com/spachecor/componenteschulos/MainActivity.kt
@@ -4,21 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            body()
+            FlexColumnExample()
         }
     }
 }
@@ -32,9 +33,34 @@ fun body() {
         Text("Hola terrible")
     }
 }
+@Composable
+fun FlexRowExample() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween // Espacio entre elementos
+    ) {
+        Box(modifier = Modifier.weight(1f).height(50.dp).background(Color.Red))
+        Box(modifier = Modifier.weight(1f).height(50.dp).background(Color.Green))
+        Box(modifier = Modifier.weight(1f).height(50.dp).background(Color.Blue))
+    }
+}
+
+@Composable
+fun FlexColumnExample() {
+    Column(
+        modifier = Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.SpaceBetween, // Espacio entre elementos
+        horizontalAlignment = Alignment.CenterHorizontally // Alineación horizontal
+    ) {
+        Box(modifier = Modifier.weight(1f).fillMaxWidth().background(Color.Yellow))
+        Box(modifier = Modifier.weight(3f).fillMaxWidth().background(Color.Magenta))
+        Box(modifier = Modifier.weight(1f).fillMaxWidth().background(Color.Cyan))
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    body()
+    FlexRowExample()
 }
