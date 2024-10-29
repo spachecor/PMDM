9cefc858c23833fd793a54c9797861889631b2db
Selene <spachecor03@gmail.com>
2024-10-29 14:52:13 +0100

    maniana 29102024

---
+++ SplashScreenExample/app/src/main/java/com/spachecor/splashscreenexample/MainScreen.kt
@@ -0,0 +1,27 @@
+package com.spachecor.splashscreenexample
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+    @Composable
+    fun MainScreen(){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Pantalla ppal")
+        }
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun MainScreenPreview(){
+        MainScreen()
+    }
